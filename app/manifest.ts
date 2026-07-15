27c7c5ef572dfd8725640c71ced84dd995a49ce4
usersessions <info@usersessions.io>
2026-07-15 10:24:25 +0300

    chore: use WTMF logo for favicon and PWA icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,9 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#030712',
     icons: [
       {
-        src: '/icon-192x192.svg',
-        sizes: '192x192',
-        type: 'image/svg+xml',
-      },
-      {
-        src: '/icon-512x512.svg',
-        sizes: '512x512',
-        type: 'image/svg+xml',
+        src: '/logo.png',
+        sizes: 'any',
+        type: 'image/png',
       },
     ],
   };
